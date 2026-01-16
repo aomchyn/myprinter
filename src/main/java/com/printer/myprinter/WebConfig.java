bf45fe1d5661e38132fb025b57c6a3b0a124841e
aomchyn <orm.mee19@gmail.com>
2026-01-16 12:38:20 +0700

    Intail commit

---
+++ src/main/java/com/printer/myprinter/WebConfig.java
@@ -0,0 +1,48 @@
+package com.printer.myprinter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.printer.myprinter.interceptor.JwtInterceptor;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+    private static String secret;
+
+    public WebConfig (JwtInterceptor jwtInterceptor){
+        this.jwtInterceptor = jwtInterceptor;
+    }
+
+    @Override
+    public void addInterceptors (InterceptorRegistry registry){
+        registry.addInterceptor(jwtInterceptor);
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/(**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*");
+    }
+
+    public static String getSecret (){
+        if (secret == null){
+            Dotenv dotenv = Dotenv.configure()
+            .directory(System.getProperty("user.dir")+ "/myprinter")
+            .load();
+
+            secret = dotenv.get("JWT_SECRET");
+        }
+
+        return secret;
+    }
+    
+}
---
+++ src/main/java/com/printer/myprinter/interceptor/JwtInterceptor.java
@@ -0,0 +1,59 @@
+package com.printer.myprinter.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.printer.myprinter.WebConfig;
+import com.printer.myprinter.annotation.RequireAuth;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor{
+
+    @Override
+    public boolean preHandle (
+        HttpServletRequest request,
+        HttpServletResponse response, Object handler)
+        throws Exception{
+            if (request.getMethod().equals("OPTIONS")){
+                return true;
+            }
+
+        if (!(handler instanceof HandlerMethod)){
+            return true;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+        boolean hasRequireAuth = handlerMethod.getMethodAnnotation(RequireAuth.class) != null;
+
+        if (!hasRequireAuth){
+            return true;
+        }
+
+        String token = request.getHeader("Authorization");
+
+        if (token == null || !token.startsWith("Bearer")){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        try {
+            String tokenWithOutBearer = token.replace("Bearer", "");
+            JWT.require(Algorithm.HMAC256(WebConfig.getSecret()))
+            .build()
+            .verify(tokenWithOutBearer);
+
+            return true;
+        } catch (Exception e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+        }
+    
+}
