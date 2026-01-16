b25f650fd9a6023e986a7cf3156f2625df53c475
aomchyn <orm.mee19@gmail.com>
2026-01-16 10:07:55 +0700

    Initail commit

---
+++ src/main/java/com/printer/myprinter/MyprinterApplication.java
@@ -0,0 +1,26 @@
+package com.printer.myprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+@RestController
+@ComponentScan (basePackages = "com.printer.myprinter")
+public class MyprinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprinterApplication.class, args);
+	}
+
+    @GetMapping("/hello")
+    public String hello(){
+		return"Hello world SpringBoot Printer!";
+	}
+    
+
+}
---
+++ src/test/java/com/printer/myprinter/MyprinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printer.myprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
